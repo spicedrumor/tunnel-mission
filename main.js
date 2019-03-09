fc1b5bf6b1b0baa99a26d6b52ca6a8caec7ba0a6
spicedrumor <notreal@email.sorry>
2019-03-08 21:32:34 -0600

    Test.

--- main.js
+++ main.js
@@ -1558,7 +1558,7 @@ function newGame() {
         newMessage("");
     }
 
-    newMessage("Tunnel Runner version 0.44a.");
+    newMessage("Tunnel Runner version 0.44b.");
 
     newMessage("Type \"h\" at any time for help.");
 
@@ -1700,19 +1700,19 @@ var catcher = function(evt) {
 elm.addEventListener('touchstart', catcher, true);
 
 // disable double-tap zoom:
-function preventZoom(e) {
-  var t2 = e.timeStamp;
-  var t1 = e.currentTarget.dataset.lastTouch || t2;
-  var dt = t2 - t1;
-  var fingers = e.touches.length;
-  e.currentTarget.dataset.lastTouch = t2;
-
-  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-
-  e.preventDefault();
-  e.target.click();
-}
-context.canvas.addEventListener('touchstart', preventZoom); 
+//function preventZoom(e) {
+//  var t2 = e.timeStamp;
+//  var t1 = e.currentTarget.dataset.lastTouch || t2;
+//  var dt = t2 - t1;
+//  var fingers = e.touches.length;
+//  e.currentTarget.dataset.lastTouch = t2;
+//
+//  if (!dt || dt > 500 || fingers > 1) return; // not double-tap
+//
+//  e.preventDefault();
+//  e.target.click();
+//}
+//context.canvas.addEventListener('touchstart', preventZoom); 
 
 startGame();
 
